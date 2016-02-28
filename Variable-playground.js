acf5c417614a05a80742f957aeffcec5d1e3ce05
Jake Holmes <jholmes218@hotmail.com>
2016-02-28 14:00:17 -0800

    add Variable playground area

---
+++ Variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+// 	name: 'Jake',
+// 	age: 22
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Jake',
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+// Array example
+
+var grades = [15, 88];
+
+function addGrade (number) {
+	grades.push(number);
+}
+
+addGrade(29);
+console.log(grades);
